2984fcc2b5c1e10a5e932f6a1d9a9c5bd35db216
cameron-sudo169 <cameronsnowdon@benifex.com>
2026-03-28 11:07:43 +0000

    Delete src/components directory

--- src/components/ProgressBar.jsx
+++
@@ -1,9 +0,0 @@
-export default function ProgressBar({ listened, total }) {
-  const pct = (listened / total) * 100;
-
-  return (
-    <div className="progress-bar">
-      <div className="progress-fill" style={{ width: `${pct}%` }} />
-    </div>
-  );
-}
